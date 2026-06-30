d33f8ad1b4d0a6d9285523d8f8f15709aaaa6fcd
varshil13 <shahvarshil13022000@gmail.com>
2026-06-30 15:16:48 +0530

    Initial Commit

---
+++ account-service/account-service/src/test/java/com/example/disributed_lovable/AccountService/account_service/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.disributed_lovable.AccountService.account_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ workspace-service/workspace-service/src/test/java/com/example/disributed_lovable/WorkspaceService/workspace_service/WorkspaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.disributed_lovable.WorkspaceService.workspace_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
