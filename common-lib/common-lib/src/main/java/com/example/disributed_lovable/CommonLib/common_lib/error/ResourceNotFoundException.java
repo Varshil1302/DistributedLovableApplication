8746b767511dd3e62282b5a9a6b03ef543adcbd9
varshil13 <shahvarshil13022000@gmail.com>
2026-06-30 17:35:58 +0530

    Add some files in common-lib which are used by every downstram service.

---
+++ common-lib/common-lib/src/main/java/com/example/disributed_lovable/CommonLib/common_lib/error/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.disributed_lovable.CommonLib.common_lib.error;
+
+public class ResourceNotFoundException extends RuntimeException
+{
+
+    public ResourceNotFoundException(String message)
+    {
+        super(message);
+    }
+}
---
+++ common-lib/common-lib/src/main/java/com/example/disributed_lovable/CommonLib/common_lib/security/JwtAuthFilter.java
@@ -0,0 +1,60 @@
+package com.example.disributed_lovable.CommonLib.common_lib.security;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
+@Slf4j
+public class JwtAuthFilter extends OncePerRequestFilter
+{
+
+    JwtService jwtService;
+    HandlerExceptionResolver handlerExceptionResolver;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException
+    {
+        try {
+            final String authHeader = request.getHeader("Authorization");
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            final String jwttoken = authHeader.split(" ")[1];
+            log.info("Jwt Token:::{}", jwttoken);
+            JwtUserPrincipal user = jwtService.validateToken(jwttoken);
+            log.info("User :::{}", user);
+            if (user != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                //User u1=userRepository.findById(userId).get();
+                UsernamePasswordAuthenticationToken userToken = new UsernamePasswordAuthenticationToken(user, null, user.authorityList());
+                SecurityContextHolder.getContext().setAuthentication(userToken);
+            }
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            handlerExceptionResolver.resolveException(request,response,null,e);
+        }
+    }
+
+    @Override
+    protected boolean shouldNotFilterAsyncDispatch() {
+        return true;
+    }
+}
